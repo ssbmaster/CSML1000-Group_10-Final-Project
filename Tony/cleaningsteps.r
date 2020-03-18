aa1fff20ec71ae2ccadc6798f97efc6c1fa1489c
ssbmaster <22460852+ssbmaster@users.noreply.github.com>
2020-03-17 20:47:28 -0400

    k=2, k=3, k=4 plots added and names updated for most of everything required. Code updated to generate them.

--- Tony/cleaningsteps.r
+++ Tony/cleaningsteps.r
@@ -70,17 +70,30 @@ cleanNoNASecondsClockData <- cleanNoNASecondsClockData[cleanNoNASecondsClockData
 # write.csv(cleanNoNAData, "../data/shot_logs_clean_noNA.csv")
 # write.csv(cleanNoNASecondsClockData, "../data/shot_logs_clean_noNA_secondsclock.csv")
 
-ggplot(cleanNoNASecondsClockData, mapping = aes(SHOT_CLOCK, stat(count))) + geom_bar()
-ggplot(cleanNoNASecondsClockData, mapping = aes(SHOT_CLOCK, stat(count))) + geom_histogram(bins = 24)
-ggplot(cleanNoNASecondsClockData, mapping = aes(GAME_CLOCK, stat(count))) + geom_bar()
-ggplot(cleanNoNASecondsClockData, mapping = aes(GAME_CLOCK, stat(count))) + geom_histogram(bins = 36)
 
-
-
-# # We can now remove any records that have NAs
-# myDataClean <- na.omit(initialData)
-# summary(myDataClean)
-# xOnlyData <- myDataClean[, -1]
+# Plot things.
+ggplot(cleanNoNASecondsClockData, aes(SHOT_DIST)) + geom_bar() + 
+  labs(title = "Shot Distance Histogram") + xlab("Shot Distance (ft)") + ylab("Total # of shots")
+ggplot(cleanNoNASecondsClockData, aes(CLOSE_DEF_DIST)) + geom_bar() + 
+  labs(title = "Closest Defender Distance Histogram") + xlab("Closest Defender Distance (ft)") + ylab("Count")
+ggplot(cleanNoNASecondsClockData, aes(PERIOD)) + geom_bar() + 
+  labs(title = "Period Histogram") + xlab("Period") + ylab("Count")
+ggplot(cleanNoNASecondsClockData, aes(GAME_CLOCK)) + geom_bar() + 
+  labs(title = "Game Clock Histogram") + xlab("Game Clock (seconds)") + ylab("Count")
+ggplot(cleanNoNASecondsClockData, aes(SHOT_CLOCK)) + geom_bar() + 
+  labs(title = "Shot Clock Histogram") + xlab("Shot Clock (seconds)") + ylab("Count")
+ggplot(cleanNoNASecondsClockData, aes(FINAL_MARGIN)) + geom_bar() + 
+  labs(title = "Final Margin Histogram") + xlab("Final Margin") + ylab("Count")
+ggplot(cleanNoNASecondsClockData, aes(SHOT_NUMBER)) + geom_bar() + 
+  labs(title = "Shot Number Histogram") + xlab("Shot Number") + ylab("Count")
+ggplot(cleanNoNASecondsClockData, aes(DRIBBLES)) + geom_bar() + 
+  labs(title = "Dribbles Histogram") + xlab("Dribbles") + ylab("Count")
+ggplot(cleanNoNASecondsClockData, aes(TOUCH_TIME)) + geom_bar() + 
+  labs(title = "Touch Time Histogram") + xlab("Touch Time (seconds)") + ylab("Count")
+ggplot(cleanNoNASecondsClockData, aes(PTS_TYPE)) + geom_bar() + 
+  labs(title = "Points Type Histogram") + xlab("Point Type") + ylab("Count")
+ggplot(cleanNoNASecondsClockData, aes(SHOT_RESULT)) + geom_bar() + 
+  labs(title = "Shot Result Histogram") + xlab("Shot Result") + ylab("Count")
 
 
 # Get the scaled numeric-only data for use for clustering.
@@ -107,30 +120,63 @@ plot(1:k.max, wss,
      type="b", pch = 19, frame = FALSE,
      xlab="Number of clusters K",
      ylab="Total within-clusters sum of squares")
+
 # Bayesian Inference Criterion for k means to validate choice from Elbow Method
 d_clust <- Mclust(as.matrix(kdata), G=1:10,
                   modelNames = mclust.options("emModelNames"))
 d_clust$BIC
 plot(d_clust)
 
+# Let us apply kmeans for k=2 clusters 
+kmm.2 <- kmeans(kdata, 2, nstart = 50, iter.max = 15)
 # Let us apply kmeans for k=3 clusters 
 kmm.3 <- kmeans(kdata, 3, nstart = 50, iter.max = 15) 
+# Let us apply kmeans for k=3 clusters 
+kmm.4 <- kmeans(kdata, 4, nstart = 50, iter.max = 15) 
 # We keep number of iter.max=15 to ensure the algorithm converges and nstart=50 to
 # Ensure that atleat 50 random sets are choosen
+kmm.2
 kmm.3
+kmm.4
 
 # Plot the clusters
 clusplot(kdataunscaled, kmm.3$cluster, color=TRUE, shade=TRUE, labels=2, lines=0)
 
 # Centroid Plot against 1st 2 discriminant functions
+plotcluster(kdataunscaled, kmm.2$cluster)
 plotcluster(kdataunscaled, kmm.3$cluster)
+plotcluster(kdataunscaled, kmm.4$cluster)
 
-# PCA
-pca <- prcomp(kdata)
-summary(pca)
-biplot(pca, cex = c(0.01,1))
+# Plot kmeans k=2 clusters to see them
+cleanNoNASecondsClockData %>%
+  mutate(cluster = factor(kmm.2$cluster)) %>%
+  ggplot(aes(SHOT_DIST, CLOSE_DEF_DIST, color = cluster)) +
+  geom_point(position = "jitter", alpha = 0.5, size = 0.5) + facet_grid(PERIOD ~ SHOT_RESULT) +
+  labs(title = "Shot Distance vs Closest Defender Distance, by Shot Result and Period (2 Clusters)") + 
+  xlab("Shot Distance (ft)") + ylab("Closest Defender Distance")
 
-# Plot kmeans clusters to see them
+cleanNoNASecondsClockData %>%
+  mutate(cluster = factor(kmm.2$cluster)) %>%
+  ggplot(aes(DRIBBLES, SHOT_DIST, color = cluster)) +
+  geom_point(position = "jitter", alpha = 0.5, size = 0.5) + facet_grid(PERIOD ~ SHOT_RESULT) +
+  labs(title = "Dribbles vs Shot Distance, by Shot Result and Period (2 Clusters)") + 
+  ylab("Shot Distance (ft)") + xlab("# of Dribbles")
+
+cleanNoNASecondsClockData %>%
+  mutate(cluster = factor(kmm.2$cluster)) %>%
+  ggplot(aes(SHOT_DIST, CLOSE_DEF_DIST, color = cluster)) +
+  geom_point(position = "jitter", alpha = 0.5, size = 0.5) + facet_wrap(~ SHOT_RESULT) +
+  labs(title = "Shot Distance vs Closest Defender Distance, by Shot Result (2 Clusters)") + 
+  xlab("Shot Distance (ft)") + ylab("Closest Defender Distance (ft)")
+
+cleanNoNASecondsClockData %>%
+  mutate(cluster = factor(kmm.2$cluster)) %>%
+  ggplot(aes(DRIBBLES, SHOT_DIST, color = cluster)) +
+  geom_point(position = "jitter", alpha = 0.5, size = 0.5) + facet_wrap(~ SHOT_RESULT) +
+  labs(title = "Dribbles vs Shot Distance, by Shot Result (2 Clusters)") +
+  ylab("Shot Distance (ft)") + xlab("# of Dribbles")
+
+# Plot kmeans k=3 clusters to see them
 cleanNoNASecondsClockData %>%
   mutate(cluster = factor(kmm.3$cluster)) %>%
   ggplot(aes(SHOT_DIST, CLOSE_DEF_DIST, color = cluster)) +
@@ -159,28 +205,39 @@ cleanNoNASecondsClockData %>%
   labs(title = "Dribbles vs Shot Distance, by Shot Result (3 Clusters)") +
   ylab("Shot Distance (ft)") + xlab("# of Dribbles")
 
-ggplot(cleanNoNASecondsClockData, aes(SHOT_DIST)) + geom_bar() + 
-  labs(title = "Shot Distance Histogram") + xlab("Shot Distance (ft)") + ylab("Total # of shots")
-ggplot(cleanNoNASecondsClockData, aes(CLOSE_DEF_DIST)) + geom_bar() + 
-  labs(title = "Closest Defender Distance Histogram") + xlab("Closest Defender Distance (ft)") + ylab("Count")
-ggplot(cleanNoNASecondsClockData, aes(PERIOD)) + geom_bar() + 
-  labs(title = "Period Histogram") + xlab("Period") + ylab("Count")
-ggplot(cleanNoNASecondsClockData, aes(GAME_CLOCK)) + geom_bar() + 
-  labs(title = "Game Clock Histogram") + xlab("Game Clock (seconds)") + ylab("Count")
-ggplot(cleanNoNASecondsClockData, aes(SHOT_CLOCK)) + geom_bar() + 
-  labs(title = "Shot Clock Histogram") + xlab("Shot Clock (seconds)") + ylab("Count")
-ggplot(cleanNoNASecondsClockData, aes(FINAL_MARGIN)) + geom_bar() + 
-  labs(title = "Final Margin Histogram") + xlab("Final Margin") + ylab("Count")
-ggplot(cleanNoNASecondsClockData, aes(SHOT_NUMBER)) + geom_bar() + 
-  labs(title = "Shot Number Histogram") + xlab("Shot Number") + ylab("Count")
-ggplot(cleanNoNASecondsClockData, aes(DRIBBLES)) + geom_bar() + 
-  labs(title = "Dribbles Histogram") + xlab("Dribbles") + ylab("Count")
-ggplot(cleanNoNASecondsClockData, aes(TOUCH_TIME)) + geom_bar() + 
-  labs(title = "Touch Time Histogram") + xlab("Touch Time (seconds)") + ylab("Count")
-ggplot(cleanNoNASecondsClockData, aes(PTS_TYPE)) + geom_bar() + 
-  labs(title = "Points Type Histogram") + xlab("Point Type") + ylab("Count")
-ggplot(cleanNoNASecondsClockData, aes(SHOT_RESULT)) + geom_bar() + 
-  labs(title = "Shot Result Histogram") + xlab("Shot Result") + ylab("Count")
+# Plot kmeans k=4 clusters to see them
+cleanNoNASecondsClockData %>%
+  mutate(cluster = factor(kmm.4$cluster)) %>%
+  ggplot(aes(SHOT_DIST, CLOSE_DEF_DIST, color = cluster)) +
+  geom_point(position = "jitter", alpha = 0.5, size = 0.5) + facet_grid(PERIOD ~ SHOT_RESULT) +
+  labs(title = "Shot Distance vs Closest Defender Distance, by Shot Result and Period (4 Clusters)") + 
+  xlab("Shot Distance (ft)") + ylab("Closest Defender Distance")
+
+cleanNoNASecondsClockData %>%
+  mutate(cluster = factor(kmm.4$cluster)) %>%
+  ggplot(aes(DRIBBLES, SHOT_DIST, color = cluster)) +
+  geom_point(position = "jitter", alpha = 0.5, size = 0.5) + facet_grid(PERIOD ~ SHOT_RESULT) +
+  labs(title = "Dribbles vs Shot Distance, by Shot Result and Period (4 Clusters)") + 
+  ylab("Shot Distance (ft)") + xlab("# of Dribbles")
+
+cleanNoNASecondsClockData %>%
+  mutate(cluster = factor(kmm.4$cluster)) %>%
+  ggplot(aes(SHOT_DIST, CLOSE_DEF_DIST, color = cluster)) +
+  geom_point(position = "jitter", alpha = 0.5, size = 0.5) + facet_wrap(~ SHOT_RESULT) +
+  labs(title = "Shot Distance vs Closest Defender Distance, by Shot Result (4 Clusters)") + 
+  xlab("Shot Distance (ft)") + ylab("Closest Defender Distance (ft)")
+
+cleanNoNASecondsClockData %>%
+  mutate(cluster = factor(kmm.4$cluster)) %>%
+  ggplot(aes(DRIBBLES, SHOT_DIST, color = cluster)) +
+  geom_point(position = "jitter", alpha = 0.5, size = 0.5) + facet_wrap(~ SHOT_RESULT) +
+  labs(title = "Dribbles vs Shot Distance, by Shot Result (4 Clusters)") +
+  ylab("Shot Distance (ft)") + xlab("# of Dribbles")
+
+# PCA
+pca <- prcomp(kdata)
+summary(pca)
+biplot(pca, cex = c(0.01,1))
 
 # # Hierarchical Agglomerative
 # d <- dist(kdata, method = "euclidean") # distance matrix
